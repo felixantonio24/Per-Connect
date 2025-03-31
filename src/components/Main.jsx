bcb0c28d7aa85ea619bfde39be8052077574e807
Felix <felixantonio-dev@hotmail.com>
2025-03-31 00:21:25 -0500

    procs-main-line-18

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,17 +2,31 @@ import Maintyle from '../Style/Main.module.css';
 const Main = ()=>{
     return(
         <>
-        <main>
-            <div className={Maintyle.containerMain}>  
-                <div>
-                    <h2>Discover Peru Connect's Mission</h2>
-                </div>
-                <div>
-                    <h3>Peru Connect is dedicated to connecting users with the vibrant culture and safety insights of Peru. Our mission is to provide a comprehensive platform that fosters community engagement and cultural appreciation. We envision being the leading digital media platform for anyone interested in exploring Peru, ensuring that travelers and residents alike have access to essential information. Key features include an interactive map for safety, user-generated content for shared experiences, and up-to-date news on tourism and gastronomy. Join us in cultivating a vibrant community that celebrates the beauty of Peru.
-                    </h3>
-                </div>
+  
+  <main>
+    <section> 
+        <div className={Maintyle.containerMain}>  
+            <div className={Maintyle.bloqueuno}>
+                <h2>Discover Peru Connect's Mission</h2>
             </div>
-        </main>
+            <div className={Maintyle.bloquedos}>
+                <p>Peru Connect is dedicated to connecting users with the vibrant culture and safety insights of Peru. Our mission is to provide a comprehensive platform that fosters community engagement and cultural appreciation. We envision being the leading digital media platform for anyone interested in exploring Peru, ensuring that travelers and residents alike have access to essential information. Key features include an interactive map for safety, user-generated content for shared experiences, and up-to-date news on tourism and gastronomy. Join us in cultivating a vibrant community that celebrates the beauty of Peru.</p>
+            </div>
+        </div>
+    </section>
+    
+    <section>
+        <div className={Maintyle.containerdos}>
+            <div className={Maintyle.containerbloqueuno}>
+                <h2>Discover the Diverse Regions of Peru </h2>
+            </div>
+            <div className={Maintyle.containerbloquedos}>
+                <p>Explore the diverse regions of Peru, each offering unique attractions and experiences. From the coastal deserts to the Andean highlands and the Amazon rainforest, Peru Connect provides insights into the best destinations, activities, and cultural experiences in each region. Plan your adventure with us!</p>
+            </div>
+        </div>
+    </section>
+</main>
+
         </>
     )
 }
