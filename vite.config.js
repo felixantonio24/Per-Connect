f327ef67cec2f3fe74f2d99c172732083942d9c7
Felix <felixantonio-dev@hotmail.com>
2025-06-13 13:49:51 -0500

    Instalar @vitejs/plugin-react

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Per-Connect/', // Ajusta según el nombre de tu repositorio
+  build: {
+    outDir: 'docs', // Usa 'docs' para GitHub Pages
+  },
 });
