2c8ba9b9e5c21e39bc37d7c5c8a9171add7f3c21
Felix <felixantonio-dev@hotmail.com>
2025-06-13 13:41:58 -0500

    proscs-error-complet-img-white-page-1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Per-Connect/',
   plugins: [react()],
-})
+  base: '/Per-Connect/', // Ajusta según el nombre de tu repositorio
+});
